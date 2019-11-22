ffbac233531cddd86f475b1c7945c01f98e9867d
Nicole <nicoleotieno18@gmail.com>
2019-11-22 16:36:58 +0300

    create about page

--- src/components/App.js
+++ src/components/App.js
@@ -9,17 +9,18 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import Dashboard from "./Dashboard/Dashboard.jsx";
 import Header from "./Header/Header.jsx";
+import About from "./About/About.jsx";
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Dashboard />
+      {/* <Dashboard /> */}
       <Switch>
-        {/* <Route exact path="/" component={Dashboard} /> */}
-        {/* <Route path="/about" component={AboutPage} />
-        <Route path="/courses" component={CoursesPage} />
-        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/about" component={About} />
+        {/* <Route path="/courses" component={CoursesPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} /> */}
 
         {/* The route /course/:slug should come first before the /course route to avooid premature matching */}
         {/* <Route path="/course" component={ManageCoursePage} /> */}
